f352b951ed4991858e30928f5df9d251a0947c9c
cyjm <ivan.korobkin@protonmail.com>
2018-04-10 14:14:46 +0300

    Added functionality for creating new blogs

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from src.common.database import Database
 from src.models.blog import Blog
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, make_response
 from src.models.user import User
 
 __author__ = 'cyjm'
@@ -59,6 +59,20 @@ def user_blogs(user_id = None):
     blogs = user.get_blogs()
 
     return  render_template('user_blogs.html', blogs=blogs, email=user.email)
+
+@app.route('/blogs/new' , methods=['POST', 'GET'])
+def create_new_blog():
+    if request.method == 'GET':
+        return render_template('new_blog.html')
+    else:
+        title = request.form['title']
+        description = request.form['description']
+        user = User.get_by_email(session['email'])
+
+        new_blog = Blog(user.email, title, description, user._id)
+        new_blog.save_to_mongo()
+
+        return make_response(user_blogs(user._id))
 
 @app.route('/posts/<string:blog_id>')
 def blog_posts(blog_id):
